567d5f947843d3aa860a7edc37fbf7b0ed860737
jerry12543 <jerryzhu@umich.edu>
2024-12-28 22:32:48 -0500

    Update fast.cpp

--- 1.TwoSum/fast.cpp
+++ 1.TwoSum/fast.cpp
@@ -7,10 +7,10 @@ public:
         unordered_map<int, int> complements;
         complements.reserve(nums.size());
         for (size_t i = 0; i < nums.size(); ++i) {
-            if (complements.find(target - nums[i]) != complements.end()) {
+            if (complements.find(target - nums[i]) != complements.end()) { // case 1
                 return { complements[target - nums[i]], static_cast<int>(i) };
             }
-            complements[nums[i]] = static_cast<int>(i);
+            complements[nums[i]] = static_cast<int>(i); // case 2
         }
         return { -1, -1 };
     }
