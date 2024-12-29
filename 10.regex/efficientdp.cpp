ddaab5d581aac370bd06740cfea8b6fdf4e9eb02
jerry12543 <jerryzhu@umich.edu>
2024-12-28 22:28:39 -0500

    Update efficientdp.cpp

--- 10.regex/efficientdp.cpp
+++ 10.regex/efficientdp.cpp
@@ -7,6 +7,7 @@ public:
     static auto isMatch(string const& s, string const& p) -> bool {
         vector<bool> prev(p.length() + 1);
         vector<bool> curr(p.length() + 1);
+        // base cases
         prev[0] = true;
         for (size_t i = 1; i < p.length() && p[i] == '*'; i += 2) {
             prev[i] = true;
@@ -15,9 +16,9 @@ public:
         for (char const currchar : s) {
             fill(curr.begin(), curr.end(), false);
             for (size_t pind = 0; pind < p.length(); ++pind) {
-                if (currchar == p[pind] || p[pind] == '.') {
+                if (currchar == p[pind] || p[pind] == '.') { // case 1
                     curr[pind + 1] = prev[pind];
-                } else if (p[pind] == '*') {
+                } else if (p[pind] == '*') { // case 2
                     curr[pind + 1]
                       = curr[pind - 1]                                                         // match with none
                      || (prev[pind + 1] && (currchar == p[pind - 1] || p[pind - 1] == '.'));   // match with 1+
