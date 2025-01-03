e436d6014f3e75fe864ed5a548dd859767425c67
jerry12543 <jerryzhu@umich.edu>
2025-01-02 19:31:29 -0500

    adding two numbers

---
+++ 0002.AddTwoNumbers/intuition.md
@@ -0,0 +1,6 @@
+Add Two Numbers Intuition
+==========================
+We will process the digits of the numbers and add them together one by one, starting from the least significant digit. For each pair of digits, if their sum is larger than 9, we will store the excess in a carry integer, which will be added to the next pair of digits in subsequent iterations.
+Notice that when adding two numbers, the resulting number can have at most one more digit than the longer of the two numbers. This happens when the final sum includes a leftover carry. Thus, after processing all digits, we will add another node to our sum if our carry is 1.
+
+We stop looping after going through all of the digits of the longer number, so our solution is O(max($m,n$)).We will add at most 1 extra node, so we are using O(1) memory.
---
+++ 0002.AddTwoNumbers/optimal.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode()
+        : val(0)
+        , next(nullptr) {}
+    ListNode(int x)
+        : val(x)
+        , next(nullptr) {}
+    ListNode(int x, ListNode* next)
+        : val(x)
+        , next(next) {}
+};
+
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        // make sure l1 is longer than l2
+        for (ListNode *l1ptr = l1, *l2ptr = l2; l2ptr; l1ptr = l1ptr->next, l2ptr = l2ptr->next) {
+            if (!l1ptr) {
+                swap(l1, l2);
+                break;
+            }
+        }
+
+        int carry = 0;
+        auto* head = l1;
+        ListNode* prev = nullptr;
+
+        for (; l1; l1 = l1->next, l2 = (!l2) ? l2 : l2->next) {
+            int const l1val = l1->val;
+            int const l2val = !l2 ? 0 : l2->val;
+            int const val = l1val + l2val + carry;
+            carry = (val >= 10) ? 1 : 0;
+
+            l1->val = val % 10;
+            prev = l1;
+        }
+        if (carry != 0) {
+            prev->next = new ListNode(carry);
+        }
+        return head;
+    }
+};
